07b7f1a6d3954b5a04accaa95b9ec39e17057df6
Lívia Calderan <liv_calderan@hotmail.com>
2024-01-24 12:49:56 -0400

    Aula herança - ainda não está executando o ataque como deveria

--- src/orientacaoObjetos/heranca/Jogador.java
+++ src/orientacaoObjetos/heranca/Jogador.java
@@ -3,7 +3,24 @@ public class Jogador {
     
     int x;
     int y; //posições
+    int vida = 100;
 
+
+    boolean atacar(Jogador oponente) {
+        int deltaX = Math.abs(x - oponente.x);
+        int deltaY = Math.abs(y - oponente.y);
+
+        if (deltaX == 0 && deltaY == 1) {
+            oponente.vida-= 10;
+            return true;
+        } else if (deltaX == 1 && deltaY == 0){
+            oponente.vida -= 10;
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
     boolean andar(Direcoes direcao){
 
         switch (direcao) {
--- src/orientacaoObjetos/heranca/Jogo.java
+++ src/orientacaoObjetos/heranca/Jogo.java
@@ -5,13 +5,20 @@ public class Jogo {
         
         Jogador j1 = new Jogador();
         j1.x = 10;
-        j1.y = 20;
-        j1.andar(Direcoes.NORTE);
-        j1.andar(Direcoes.OESTE);
-        j1.andar(Direcoes.NORTE);
-        j1.andar(Direcoes.OESTE);
+        j1.y = 10;
+        
+        Jogador j2 = new Jogador();
+        j1.x = 11;
+        j2.y = 10;
 
-        System.out.println(j1.y);
-        System.out.println(j1.x);
+        System.out.println(j1.vida);
+        System.out.println(j2.vida);
+
+        j1.atacar(j2);
+        j2.atacar(j1);
+        j1.atacar(j2);
+
+        System.out.println(j1.vida);
+        System.out.println(j2.vida);
     }
 }
