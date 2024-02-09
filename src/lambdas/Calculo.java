c66470e1c265bf5658e7843b3bd7bdbed7ed17b3
Lívia Calderan <liv_calderan@hotmail.com>
2024-02-08 21:05:15 -0400

    Interfaces Funcionais #1

--- src/lambdas/Calculo.java
+++ src/lambdas/Calculo.java
@@ -1,6 +1,8 @@
 package lambdas;
 
+@FunctionalInterface //força a ter um unico metodo
 public interface Calculo {
     
     public abstract double excutar(double a, double b);
+    // double teste(); - invalido pois não fica uma interface funcional; Erro
 }
