dbbe4d6f2b883bdff1e35aa09a2d9f337267cbfe
Lívia Calderan <liv_calderan@hotmail.com>
2024-02-08 21:11:47 -0400

    Interface #2

--- src/lambdas/Calculo.java
+++ src/lambdas/Calculo.java
@@ -3,14 +3,14 @@ package lambdas;
 @FunctionalInterface //força a ter um unico metodo
 public interface Calculo {
     
-    public double excutar(double a, double b);
+    public double excutar(double a, double b); //abstract implicito
     // double teste(); - invalido pois não fica uma interface funcional; Erro
 
     default String legal() { //default não da erro
         return "legal";
     }
 
-    static String massa() { //static tbm não da erro
+    static String massa() { //static tbm não da erro - passa a ser da classe
         return "Muito Legal!";
     }
 }
