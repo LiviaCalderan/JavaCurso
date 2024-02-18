050e90b8599c7f1976159cc4e4f374c8aaaff7f4
Lívia Calderan <liv_calderan@hotmail.com>
2024-02-18 18:01:27 -0400

    Reduce #03

--- src/streams/Aluno.java
+++ src/streams/Aluno.java
@@ -6,6 +6,10 @@ public class Aluno {
     final Double nota;
     final boolean bomComportamento;
     
+    public Aluno(String nome, Double nota) {
+       this(nome, nota, true);
+    }
+
     public Aluno(String nome, Double nota, boolean bomComportamento) {
         this.nome = nome;
         this.nota = nota;
