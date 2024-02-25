df3e0bbd5b447e3b73f9fa05d7e8927cc871bdfa
Lívia Calderan <liv_calderan@hotmail.com>
2024-02-25 18:10:47 -0400

    https://github.com/LiviaCalderan/JavaCurso.git

---
+++ src/Generics/CaixaObjetoTeste.java
@@ -0,0 +1,19 @@
+package Generics;
+
+public class CaixaObjetoTeste {
+    
+    public static void main(String[] args) {
+        
+        CaixaObjeto caixaA = new CaixaObjeto();
+        caixaA.guardar(2.3); 
+
+        Double coisa = (Double) caixaA.abrir();
+        System.out.println(coisa);
+
+        CaixaObjeto caixaB = new CaixaObjeto();
+        caixaB.guardar("Oie :)"); 
+
+        String coisaB = (String) caixaB.abrir(); //conversões - maior risco de expections
+        System.out.println(coisaB);
+    }
+}
---
+++ src/Generics/CaixaTeste.java
@@ -0,0 +1,18 @@
+package Generics;
+
+public class CaixaTeste {
+    public static void main(String[] args) {
+        
+        Caixa<String> caixaA = new Caixa<String>();
+        caixaA.guardar("Segredo");
+
+        String coisaA = caixaA.abrir();
+        System.out.println(coisaA);
+
+        Caixa<Double> caixaB = new Caixa<>();
+        caixaB.guardar(4.5);
+
+        Double coisaB = caixaB.abrir();
+        System.out.println(coisaB);
+    }
+}
